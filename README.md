0038ae567b869f3da950316f00149c22a71b8269
Huzaifa <huzaifaj530@gmail.com>
2023-01-03 21:18:18 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CS232_Project_BinaryBeats
+This is my Database Management System Course Project.
+I have created a Django Project. It is a website for a gift shop.
+
+Front-end is created using HTML, CSS and 
+
+Back-end is created using DJANGO
+
+DataBase used is sqlite
+
+To run this app on your system clone this repository and type "python main.py runserver" on your terminal
+
+Make sure Django is installed on your system
---
+++ home/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+urlpatterns = [
+    path("", views.index, name="gift"),
+    path("about", views.about, name="about"),
+    path("contact ", views.contact, name="contact"),
+    path("watch", views.watch, name="watch"),
+    path("wallet", views.wallet, name="wallet"),
+    path("perfume", views.perfume, name="perfume"),
+    path("BuyNow", views.buy, name="BuyNow"),
+    path("c", views.c, name="c"),
+    path("cart", views.cart, name="cart"),
+    path("update_item", views.updateItem, name="update_item"),
+
+]
